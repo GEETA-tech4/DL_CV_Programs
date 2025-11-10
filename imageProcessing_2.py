a1a661b7a034bf492a8239fe3d11d9b550e8fe38
GEETA-tech4 <geetazine@gmail.com>
2025-11-10 11:11:08 +0530

    Update imageProcessing_2.py

--- imageProcessing_2.py
+++ imageProcessing_2.py
@@ -1,25 +1,32 @@
 import numpy as np
 import cv2
-
-imgOriginal = cv2.imread("./images/sample.jpg")
+from google.colab.patches import cv2_imshow
+imgOriginal = cv2.imread("./sample.jpeg")
 img = cv2.resize(imgOriginal, (500,500))
-cv2.imshow("Original Image",img)
+cv2_imshow(img)
+
 imgc = img.copy()
 croppedImg = imgc[50:250, 0:500]
-cv2.imshow("Cropped Image",croppedImg)
+cv2_imshow(croppedImg)
+
 resizedImg = cv2.resize(img, (1000,700))
-cv2.imshow("Resized Image : ",resizedImg)
+cv2_imshow(resizedImg)
+
 grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Gray Image",grayImg)
+cv2_imshow(grayImg)
+
 retval,thresh = cv2.threshold(grayImg, 120, 255, cv2.THRESH_BINARY)
-cv2.imshow("Threshold Image", thresh)
+cv2_imshow(thresh)
+
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 contour_img = img.copy()
 cv2.drawContours(contour_img, contours, -1, (255,0,0), 2)
-cv2.imshow("Contour Detection", contour_img)
+cv2_imshow(contour_img)
+
 detector = cv2.SimpleBlobDetector_create()
 keypoints = detector.detect(grayImg)
 blob_img = cv2.drawKeypoints(img, keypoints, np.array([]), (0, 0, 255),
                              cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-cv2.imshow("Blob Detection", blob_img)
+cv2_imshow(blob_img)
+
 cv2.waitKey(0)
